38186e65ce56873d643f118dc0734e986d4f16a5
Rafajorda <rafajorgis@gmail.com>
2026-02-03 19:51:23 +0100

    feat: Crear hooks React Query para API
    - useProductsQuery: CRUD completo de productos
    - useCategoriesQuery: CRUD completo de categorías
    - useColorsQuery: CRUD completo de colores
    - useUsersQuery: CRUD completo de usuarios
    - Incluir mutations para crear, actualizar, eliminar
    - Incluir toggle de estados y upload/delete de modelos 3D
    - Invalidación automática de cache tras mutaciones

---
+++ src/hooks/queries/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Barrel export para todos los hooks de React Query
+ */
+
+export * from './useProductsQuery';
+export * from './useCategoriesQuery';
+export * from './useColorsQuery';
+export * from './useUsersQuery';
